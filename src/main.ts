801b0d96367285a3bf89ae6afb98074ef8dea490
Dillon Kearns <dillon@dillonkearns.com>
2020-03-14 16:17:51 -0700

    Generate elm docs.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {exec} from '@actions/exec'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    core.startGroup('Generate elm package docs')
+    await exec('npx elm make --docs docs.json')
+    core.endGroup()
   } catch (error) {
     core.setFailed(error.message)
   }
