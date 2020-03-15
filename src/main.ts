1494a595512a8a26311164c834dabe145a4c3336
Dillon Kearns <dillon@dillonkearns.com>
2020-03-14 17:42:51 -0700

    Update script.

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,35 @@
 import * as core from '@actions/core'
-import {exec} from '@actions/exec'
+// import {exec} from '@actions/exec'
+import {default as axios} from 'axios'
+import {Toolkit} from 'actions-toolkit'
+const tools = new Toolkit()
 
 async function run(): Promise<void> {
   try {
-    core.startGroup('Generate elm package docs')
-    await exec('npx elm make --docs docs.json')
-    core.endGroup()
-    core.startGroup('elm-format --validate')
-    await exec('npx elm-format --validate')
-    core.endGroup()
-    core.startGroup('elm-test')
-    await exec('npx elm-test')
-    core.endGroup()
+    const githubRepo = process.env['GITHUB_REPOSITORY'] || ''
+    // 'https://package.elm-lang.org/packages/dillonkearns/elm-pages/releases.json'
+
+    // Make a request for a user with a given ID
+    console.log(
+      `https://package.elm-lang.org/packages/${githubRepo}/releases.json`
+    )
+    const versionsResponse = await axios.get(
+      `https://package.elm-lang.org/packages/${githubRepo}/releases.json`
+    )
+    const elmVersion = JSON.parse(tools.getFile('elm.json')).version
+    console.log('elmVersion', elmVersion)
+    console.log('versionsResponse', versionsResponse)
+    console.log('Version published', versionsResponse.data[elmVersion])
+
+    // core.startGroup('Generate elm package docs')
+    // await exec('npx elm make --docs docs.json')
+    // core.endGroup()
+    // core.startGroup('elm-format --validate')
+    // await exec('npx elm-format --validate')
+    // core.endGroup()
+    // core.startGroup('elm-test')
+    // await exec('npx elm-test')
+    // core.endGroup()
   } catch (error) {
     core.setFailed(error.message)
   }
